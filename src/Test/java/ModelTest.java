fa602689f1291e094d12bedabf66fb94bf37d087
Skoorps <skoorps@gmail.com>
2019-12-12 13:31:16 +0100

    Created ModelTest

---
+++ src/Test/java/ModelTest.java
@@ -0,0 +1,17 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.openjfx.Model;
+
+
+public class ModelTest {
+
+    @Test
+    void testingModelConstructor() {
+        Model output = new Model();
+        Assertions.assertNotNull(output);
+    }
+
+    //TODO Add a test for the getter
+
+
+}
