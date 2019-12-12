8a88c4eb8a0a48e81111e72be062db0ff8e96fe7
Patrick <patrick@pinyagroup.com>
2019-12-12 06:56:36 +0000

    Added classes Bank and Main

---
+++ src/main/java/org/openjfx/Bank.java
@@ -0,0 +1,13 @@
+package org.openjfx;
+
+class Bank {
+   private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public Bank(String name) {
+        this.name = name;
+    }
+}
