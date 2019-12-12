0d4e9c5fd8cf747f0f6e71fc3eef09eb3ce21152
Christoffer Clausen <christoffer.clausen@iths.se>
2019-12-12 11:36:50 +0100

    Added controller class with tests and methods for creating Person, Bank and Account

--- src/main/java/org/openjfx/Controller.java
+++ src/main/java/org/openjfx/Controller.java
@@ -1,18 +1,41 @@
 package org.openjfx;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import javafx.fxml.FXML;
+import javafx.scene.control.FocusModel;
 
-public class Contoller {
+public class Controller {
 
     private Model model;
+    private List<Bank> bankList = new ArrayList<Bank>(); //for testing
+    private List<Person> personList = new ArrayList<Person>(); //for testing
+    private List<Account> accountList = Model.getList();
 
-    public Contoller(Model model) {
+
+    public Controller(Model model) {
         this.model = model;
         createPerson();
         createBank();
         createAccount();
+    }
+
+    public List getList(String s) {
+        switch(s) {
+            case "personList":
+                return personList;
+            case "bankList":
+                return bankList;
+            case "accountList":
+                return accountList;
+            default:
+                System.out.println("Wrong input");
+        }
+    }
+
+    public Model getModel() {
+        return Model;
     }
 
     private void createPerson() {
@@ -33,7 +56,6 @@ public class Contoller {
     }
 
     private void createAccount() {
-        List<Account> accountList = Model.getList();
         accountList.add(Account account1 = new Account(1, bankA, personA));
         accountList.add(Account account2 = new Account(2, bankA, personB));
         accountList.add(Account account3 = new Account(3, bankA, personC));
