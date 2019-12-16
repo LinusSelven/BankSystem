602d208d2f8656929e60562f201b6f0bf94cbec9
Husamettin Arslan <husamettinarslan@hotmail.com>
2019-12-16 20:20:24 +0100

    Operations test modified according to new parameters

--- src/Test/java/org/openjfx/OperationsTest.java
+++ src/Test/java/org/openjfx/OperationsTest.java
@@ -15,6 +15,7 @@ class OperationsTest {
     //private  Account account = new Account(1, 100, new Person("Java", "Java", 35), new Bank("Swedbank"));
     //private  Account account2 = new Account(2, 200, new Person("Python", "Python", 25), new Bank("HSBC"));
     private List<Account> accountList= new ArrayList<>();
+    private List<String> operationList= new ArrayList<>();
     private  Operations operations;
 
     @BeforeEach
@@ -22,7 +23,7 @@ class OperationsTest {
     void init() {
         //accountList.add(account);
         //accountList.add(account2);
-        operations= new Operations(accountList);
+        operations= new Operations();
     }
 
 
@@ -30,11 +31,15 @@ class OperationsTest {
     @Order(2)
     @DisplayName("Deposit test")
     void deposit(){
+        Account account = new Account(1, 100, new Bank("Swedbank"),  new Person("Java", "Java", 35));
+        Account account2 = new Account(2, 200,  new Bank("HSBC"), new Person("Python", "Python", 25));
+        accountList.add(account);
+        accountList.add(account2);
         int index= accountList.indexOf(accountList.stream()
                 .filter(e -> e.getId() == 1)
                 .collect(Collectors.toList()).get(0));
 
-        operations.deposit(1, 500);
+        operations.deposit(1, 500, accountList, "500 sek is added", operationList);
         assertEquals(600, accountList.get(index).getBalance() );
         System.out.println("Balance after deposit = "+ accountList.get(index).getBalance());
     }
@@ -45,11 +50,16 @@ class OperationsTest {
     @Order(3)
     @DisplayName("Withdraw test")
     void withdraw(){
+        Account account = new Account(1, 100, new Bank("Swedbank"),  new Person("Java", "Java", 35));
+        Account account2 = new Account(2, 200,  new Bank("HSBC"), new Person("Python", "Python", 25));
+        accountList.add(account);
+        accountList.add(account2);
+
         int index= accountList.indexOf(accountList.stream()
                 .filter(e -> e.getId() == 1)
                 .collect(Collectors.toList()).get(0));
 
-        operations.withdraw(1, 20);
+        operations.withdraw(1, 20, accountList, "20 sek witdrawn", operationList);
         assertEquals(80, accountList.get(index).getBalance());
         System.out.println(" Balance after withdrawal = "+accountList.get(index).getBalance());
     }
@@ -59,7 +69,10 @@ class OperationsTest {
     @Order(4)
     @DisplayName("Transfer test")
     void transfer(){
-
+        Account account = new Account(1, 100, new Bank("Swedbank"),  new Person("Java", "Java", 35));
+        Account account2 = new Account(2, 200,  new Bank("HSBC"), new Person("Python", "Python", 25));
+        accountList.add(account);
+        accountList.add(account2);
         int indexAccount1= accountList.indexOf(accountList.stream()
                 .filter(e -> e.getId() == 1)
                 .collect(Collectors.toList()).get(0));
@@ -67,7 +80,7 @@ class OperationsTest {
                 .filter(e -> e.getId() == 2)
                 .collect(Collectors.toList()).get(0));
 
-        operations.transfer(1, 2 , 50);
+        operations.transfer(1, 2 , 50, accountList, "50 sek transfer from acc. 1 to acc. 2", operationList);
         assertAll(
                 ()-> assertEquals(50,accountList.get(indexAccount1).getBalance() ),
                 ()-> assertEquals(250,accountList.get(indexAccount2).getBalance() )
